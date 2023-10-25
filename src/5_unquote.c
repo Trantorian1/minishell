ace5cd4c5271d4011495ea6a3d241f6f3e9b0a64
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-25 18:48:34 +0000

    refactor(unquote): minor refactoring in unquote state

--- src/5_unquote.c
+++ src/5_unquote.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 22:44:23 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/25 00:29:37 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/25 18:47:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,15 @@
 #include "dynamic/string.h"
 #include "d_str.h"
 
-static size_t	vstr_unquote(t_vptr *_Nonnull vstr, size_t index);
+static uint8_t	unquote_impl(t_vptr *_Nonnull vstr);
 
 uint8_t	state_unquote(t_data *_Nonnull data)
 {
-	size_t	index;
-	t_str	str_curr;
-
 	if (data == NULL)
 		return (EXIT_FAILURE);
 
-	index = 0;
-	while (index < data->arg->len)
-	{
-		str_curr = vptr_get(t_str, data->arg, index);
-		if (str_eq(str_curr, QUOTE_SINGLE) || str_eq(str_curr, QUOTE_DOUBLE))
-			vstr_unquote(data->arg, index);
-		if (str_eq(str_curr, WHITESPACE))
-			vstr_rm(data->arg, index);
-		else
-			index++;
-	}
-
-	index = 0;
-	while (index < data->redir->len)
-	{
-		str_curr = vptr_get(t_str, data->redir, index);
-		if (str_eq(str_curr, QUOTE_SINGLE) || str_eq(str_curr, QUOTE_DOUBLE))
-			vstr_unquote(data->redir, index);
-		if (str_eq(str_curr, WHITESPACE))
-			vstr_rm(data->redir, index);
-		else
-			index++;
-	}
+	if (unquote_impl(data->arg) || unquote_impl(data->redir))
+		return (EXIT_FAILURE);
 
 	return (EXIT_SUCCESS);
 }
@@ -80,5 +56,28 @@ static size_t	vstr_unquote(t_vptr *_Nonnull vstr, size_t index)
 	}
 	vstr_insert(vstr, content, index);
 
+	return (EXIT_SUCCESS);
+}
+
+static uint8_t	unquote_impl(t_vptr *_Nonnull vstr)
+{
+	size_t	index;
+	t_str	str;
+
+	if (vstr == NULL)
+		return (EXIT_FAILURE);
+
+	index = 0;
+	while (index < vstr->len)
+	{
+		str = vptr_get(t_str, vstr, index);
+		if (str_eq(str, QUOTE_SINGLE) || str_eq(str, QUOTE_DOUBLE))
+			vstr_unquote(vstr, index);
+		if (str_eq(str, WHITESPACE))
+			vstr_rm(vstr, index);
+		else
+			index++;
+	}
+	
 	return (EXIT_SUCCESS);
 }
