e1643c7f6027f4b3c0f2c20fc0d430e55da2cae1
eliotmcnab@proton.me <eliotmcnab@proton.me>
2023-10-25 18:07:25 +0000

    reafactor(expand): minor refactoring in state_expand

--- src/4_expand.c
+++ src/4_expand.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 19:39:06 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/24 22:42:34 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/25 18:05:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,45 +23,15 @@
 #include "env_get.h"
 #include "d_str.h"
 
-static uint8_t	expand_string(t_str *_Nonnull str, t_vptr *_Nonnull env);
+static uint8_t	expand_impld(t_vptr *_Nonnull vptr);
 
 uint8_t	state_expand(t_data *_Nonnull data)
 {
-	size_t	index;
-	t_str	*str_curr;
-
 	if (data == NULL)
 		return (EXIT_FAILURE);
 
-	index = 0;
-	while (index < data->arg->len)
-	{
-		str_curr = vptr_get_ptr(t_str, data->arg, index);
-
-		if (str_eq(*str_curr, QUOTE_SINGLE))
-			index += 2;
-		else
-		{
-			if (expand_string(str_curr, data->env))
-				return (EXIT_FAILURE);
-			index++;
-		}
-	}
-
-	index = 0;
-	while (index < data->redir->len)
-	{
-		str_curr = vptr_get_ptr(t_str, data->redir, index);
-
-		if (str_eq(*str_curr, QUOTE_SINGLE))
-			index += 2;
-		else
-		{
-			if (expand_string(str_curr, data->env))
-				return (EXIT_FAILURE);
-			index++;
-		}
-	}
+	if (expand_impld(data->arg) || expand_impld(data->redir))
+		return (EXIT_FAILURE);
 
 	return (EXIT_SUCCESS);
 }
@@ -97,4 +67,29 @@ static uint8_t	expand_string(t_str *_Nonnull str, t_vptr *_Nonnull env)
 	str_insert_str(str, env_get(env, var.get).get, i_prev);
 
 	return (EXIT_SUCCESS);
+}
+static uint8_t	expand_impld(t_vptr *_Nonnull vptr)
+{
+	size_t	index;
+	t_str	*str;
+
+	if (vptr == NULL)
+		return (EXIT_FAILURE);
+
+	index = 0;
+	while (index < vptr->len)
+	{
+		str = vptr_get_ptr(t_str, vptr, index);
+
+		if (str_eq(*str, QUOTE_SINGLE))
+			index += 2;
+		else
+		{
+			if (expand_string(str, vptr))
+				return (EXIT_FAILURE);
+			index++;
+		}
+	}
+
+	return (EXIT_FAILURE);
 }
